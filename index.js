7399eef0e905d73a93881cf152918d007ca827ce
Yuleth Aceves <yulethaceves@gmail.com>
2023-07-14 13:14:27 -0600

    fixed indexjs

--- index.js
+++ index.js
@@ -1,43 +1,68 @@
-const svg = require ("./lib/Svg.js")
-const shape = require ("./lib/Shape.js")
-const inquirer = require ("inquirer")
-
+const SVG = require("./lib/Svg.js")
+const { Circle, Square, Triangle } = require("./lib/Shape.js")
+const inquirer = require("inquirer")
+const fs = require("fs")
 
 inquirer.prompt([
     {
-        type:"list",
-        choices:["Cirlce","Triangle","Square"],
-        name:"shape",
-        message:"Please select a shape:"
+        type: "list",
+        choices: ["Circle", "Triangle", "Square"],
+        name: "shape",
+        message: "Please select a shape:"
 
     },
 
     {
-        type:"list",
-        choices:["Red","Pink","Blue","Green"],
-        name:"color",
-        message:"Please select shape color:"
+        type: "list",
+        choices: ["Red", "Pink", "Blue", "Green"],
+        name: "Color",
+        message: "Please select shape color:"
 
     },
     {
-        type:"input",
-        name:"text",
-        message:"Please enter text max 3 characters:",
-        validate:function(txtinput){
-            if(txtinput.length > 3 || txtinput < 1){
+        type: "input",
+        name: "text",
+        message: "Please enter text max 3 characters:",
+        validate: function (txtinput) {
+            if (txtinput.length > 3 || txtinput < 1) {
                 return false
             }
             return true
         }
     },
-    
+
     {
-        type:"list",
-        choices:["Red","Pink","Blue","Green","Black", "White"],
-        name:"text color",
-        message:"Please select text color:"
+        type: "list",
+        choices: ["Red", "Pink", "Blue", "Green", "Black", "White"],
+        name: "textcolor",
+        message: "Please select text color:"
     }
-]).then (response =>{
-    console.log (response)
+]).then(response => {
+    console.log(response)
+   
+    switch (response.shape) {
+        case "Circle":
+            var logoShape = new Circle();
+            console.log(response.shape,"test")
+            break;
+        case "Triangle":
+            var logoShape = new Triangle();
+            break;
+        case "Square":
+            var logoShape = new Square();
+            break;
+    }
+    
+    console.log(logoShape)
+    logoShape.setColor(response.color);
+    var mySVG = new SVG()
+    mySVG.setText(response.text, response.textcolor)
+    mySVG.setShape(logoShape)
+    let svgcontent = mySVG.render()
+    fs.writeFileSync("logo.svg", mySVG.render(), function (err) {
+        if (err) throw err
+    })
+
+    console.log("SV", svgcontent)
 })
 
